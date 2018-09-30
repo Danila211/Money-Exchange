f318d23186d4c78db250c12dd7fd35b130180c0c
danila211 <danila-211@mail.ru>
2018-09-30 16:04:15 +0300

    makeExchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+    if (currency <= 0){
+        return{};
+    }
+   
+    if (currency > 10000 ){
+        return { error: 'You are rich, my friend! We don\'t have so much coins for exchange' };
+    }
+
+    const coins = new Map([
+    ['H',50],['Q',25],['D',10],['N',5],['P',1]
+    ]);
+
+    let result = {};
+    for (let coin of coins.keys()){
+        let grad = coins.get(coin);
+        let count = Math.floor(currency/grad);
+
+        if (count > 0) {
+            result[coin] = count;
+            currency -= grad * count;
+        }
+        
+    }
+        return result;
+    }
